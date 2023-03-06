f59d9260cd44ebe0aff143189c419994b851ab89
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-06 15:36:32 +0100

    array with rnd unique numbers

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* SIMON SAYS
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+/*
+**********
+Functions
+**********
+*/
+
+  function generateRandomNumberArray (HowMany, num, array) {
+    array.length = 0;
+    while(HowMany > array.length) {
+        let number = Math.floor(Math.random() * num);
+        if (array.indexOf(number) === -1) {
+            array.push(number);
+        };
+    };
+};
+
+/*
+**********
+Main
+**********
+*/
+let casualNumbers = [];
+
+generateRandomNumberArray (5, 100, casualNumbers);
+console.log(casualNumbers);
